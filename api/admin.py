8ba8efc93bd8da50bd7c350163d2b79ecc8972de
EdGhi <ghislain.makolle@facsciences-uy1.cm>
2023-02-16 15:59:57 +0100

    First migration with mongodb

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
-# Register your models here.
+from api.models import Note
+
+
+@admin.site.register(Note)
+class NoteAdmin(admin.ModelAdmin):
+    list_display = ('title', 'content', 'created_at', 'updated_at')
